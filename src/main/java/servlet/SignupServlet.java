51281d7e88ec021f1097a41300a2ca75f360f613
Davide Del Franco Natale <davide.delfranco44@gmail.com>
2023-06-21 11:17:06 +0200

    aggiunta connessione al DB e interazione con pagina di signup

--- src/main/java/servlet/SignupServlet.java
+++ src/main/java/servlet/SignupServlet.java
@@ -1,9 +1,13 @@
 package servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -17,25 +21,47 @@ import javax.servlet.http.HttpServletResponse;
 public class SignupServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String nome = request.getParameter("nome");
 		String cognome = request.getParameter("cognome");
 		String email = request.getParameter("email");
 		String password = request.getParameter("password");
+		Connection con = null;
+		RequestDispatcher dispatcher = null;
 		
-		String query = "INSERT INTO site_user values (?, ?, ?, ?)"; 
-		MyConnection mc = new MyConnection();
+		String query = "INSERT INTO site_user(nome,cognome,email_address,password) values(?,?,?,?)";
 		try {	 
-			PreparedStatement ps = MyConnection.getConnection().prepareStatement(query);
+			con = DbManager.getConnection();
+			PreparedStatement ps = con.prepareStatement(query);
 			ps.setString(1, nome);
 			ps.setString(2, cognome);
 			ps.setString(3, email);
 			ps.setString(4, password);
-			ps.executeUpdate();
+			
+			int rowCount = ps.executeUpdate();
+			dispatcher = request.getRequestDispatcher("./signup.jsp");
+			
+			if(rowCount > 0) {
+				request.setAttribute("status", "success");
+				 
+			}else {
+				request.setAttribute("status", "failed");
+			}
+			
+			dispatcher.forward(request, response);
+			
 		} catch (SQLException e) {
 			e.printStackTrace();
+		}finally {
+			try {
+				if(con == null)
+					return;
+				else
+					con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
 		}
 		
-		response.sendRedirect("index.jsp");
 	}
 }
--- src/main/webapp/signup.jsp
+++ src/main/webapp/signup.jsp
@@ -1,13 +1,26 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
 <jsp:include page="./header.jsp" flush="true"/>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.7.12/dist/sweetalert2.all.min.js"></script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11.7.12/dist/sweetalert2.min.css">
+<%String status = (String) request.getAttribute("status");%>
+<script type ="text/javascript">
+	if('<%= status %>' == "success"){
+			swal(
+				  'Congratulazioni!',
+				  'Sei iscritto!',
+				  'successo'
+				);
+	}
+</script>
 <body>
+	<input type="hidden" id="status" value="<%= request.getAttribute("status")%>">
 	<jsp:include page="./Nav.jsp" flush="true"/>
 	<main>
 		<section id="signup">
 		<div>		
 			<h3>Iscrizione</h3>
-			<form method="get" action="SignupServlet" name="signup" class="signup-form">				
+			<form method="POST" action="SignupServlet" name="signup" class="signup-form">				
 					<p>Nome <input type="text" name="nome"> </p>
 					<p>Cognome <input type="text" name="cognome"> </p>
 					<p>E-mail <input type=email placeholder="E-mail" name="email"> </p>
