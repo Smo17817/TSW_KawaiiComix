9307635794000f729452ca04ab5c7bfb9654e16b
Smo17817 <simone.dassisi99@gmail.com>
2023-07-04 18:41:38 +0200

    Risolto problema ricerca filtrata in check orders

--- src/main/webapp/Script/ordini.js
+++ src/main/webapp/Script/ordini.js
@@ -5,31 +5,39 @@ function annullaordine(button) {
 }
 
 function filterRows() {
-  	let input = document.getElementById("searchInput").value.toLowerCase();
-  	let startDateString = document.getElementById("startDateInput").value;
-  	let endDateString = document.getElementById("endDateInput").value;
-  	let rows = document.querySelectorAll("#container tr");
-	
+	let input = document.getElementById("searchInput").value.toLowerCase();
+	let startDateString = document.getElementById("startDateInput").value;
+	let endDateString = document.getElementById("endDateInput").value;
+	let rows = document.querySelectorAll("#container tr");
+
 	let startDate = new Date(startDateString);
 	let endDate = new Date(endDateString);
 	let formattedStartDate = formatDate(startDate);
 	let formattedEndDate = formatDate(endDate);
 
-  	for (const row of rows) {
-    	let userId = row.getAttribute("data-utente");
-    	let giorno = row.getAttribute("data-giorno");
-    	if (userId.toLowerCase().includes(input) && giorno >= formattedStartDate && giorno <= formattedEndDate) {
-    		row.style.display = "";
-    	} else {
-      		row.style.display = "none";
-    	}
-  	}
+	for (const row of rows) {
+		let userId = row.getAttribute("data-utente");
+		let giorno = row.getAttribute("data-giorno");
+		let showRow = true;
+
+		if (input && !userId.toLowerCase().includes(input)) {
+			showRow = false;
+		}
+		if (startDateString && giorno < formattedStartDate) {
+			showRow = false;
+		}
+		if (endDateString && giorno > formattedEndDate) {
+			showRow = false;
+		}
+
+		row.style.display = showRow ? "" : "none";
+	}
 }
 
 function formatDate(date) {
-  let day = String(date.getDate()).padStart(2, '0');
-  let month = String(date.getMonth() + 1).padStart(2, '0');
-  let year = date.getFullYear();
-  
-  return `${day}/${month}/${year}`;
+	let day = String(date.getDate()).padStart(2, '0');
+	let month = String(date.getMonth() + 1).padStart(2, '0');
+	let year = date.getFullYear();
+
+	return `${day}/${month}/${year}`;
 }
